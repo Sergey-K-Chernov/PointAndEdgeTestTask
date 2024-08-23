4d6cd90f23ea1ba6ec5abae1d2491c0477c01bde
Sergey-K-Chernov <sergey.k.chernov@yandex.ru>
2024-08-24 02:06:20 +0300

    Add graph generation

---
+++ PointAndEdgeTestTask/GraphGenerator.cpp
@@ -0,0 +1,39 @@
+#include "GraphGenerator.h"
+
+#include <random>
+
+const double BOTTOM = 0;
+const double TOP = 1000;
+
+Graph GenerateGraph(size_t nPoints, size_t nEdges)
+{
+	Graph graph;
+
+	std::random_device rd;
+	std::mt19937 random_generator(rd());
+	std::uniform_real_distribution<> coordinates_distribution(BOTTOM, TOP);
+	std::uniform_int_distribution<> ids_distribution(0, nPoints-1);
+
+	auto coordinate_generator = [&coordinates_distribution, &random_generator](){
+			double number;
+			do {
+				number = coordinates_distribution(random_generator);
+			} while (number == TOP);
+			return number;
+		};
+
+	for (size_t i = 0; i < nPoints; i++)
+	{
+		graph.points.push_back({ coordinate_generator(), coordinate_generator() });
+	}
+
+	for (size_t i = 0; i < nPoints; i++)
+	{
+		for (size_t j = 0; j < nEdges; j++)
+		{
+			graph.edges.push_back({static_cast<int>(i), ids_distribution(random_generator)});
+		}
+	}
+
+	return graph;
+}
---
+++ PointAndEdgeTestTask/GraphGenerator.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Geometry.h"
+
+Graph GenerateGraph(size_t nPoints, size_t nEdges);
