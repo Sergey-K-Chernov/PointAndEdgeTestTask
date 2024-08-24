ff6956e571bea3d0ffd1ca60a80a55b54af05a2a
Sergey-K-Chernov <sergey.k.chernov@yandex.ru>
2024-08-24 15:50:23 +0300

    Add a test case created from random graph

--- PointAndEdgeTestTask/TestCases.h
+++ PointAndEdgeTestTask/TestCases.h
@@ -9,3 +9,4 @@ void TestAll(std::function<Edge(const Graph&, const Point&)> func);
 void Test10PtsX1EdgeAlongXAxis(std::function<Edge(const Graph&, const Point&)> func);
 void Test10PtsX1EdgeAlongYAxis(std::function<Edge(const Graph&, const Point&)> func);
 void Test100PtsX5EdgesZigzag(std::function<Edge(const Graph&, const Point&)> func);
+void Test100PtsX5EdgesFromRandom(std::function<Edge(const Graph&, const Point&)> func);
